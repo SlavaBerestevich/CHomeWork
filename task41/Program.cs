75a74aa8b237223ea2d4d9970568b4db63bb64d2
Slava <berestevich1998@mail.rum>
2023-09-03 18:21:29 +0300

    41 задача

---
+++ task41/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+int Prompt(string massage)
+{
+    Console.Write(massage);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
